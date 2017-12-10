ddbd1f3896eca96d2a0db95376ef86ffb5828857
Simon Grundt Straubinger <simon.wismar@gmail.com>
2017-12-10 18:02:27 +0100

    Add R script

---
+++ lix.R
@@ -0,0 +1,75 @@
+# Load packages and dataset
+
+library(ggplot2)
+library(tidyr)
+
+lix <- read.csv("lix.csv")
+
+
+# Convert data from wide to long format
+
+lix <- gather(lix, type, lix, openparl_dk:newyear_royal_no, factor_key = TRUE)
+
+
+# Fig. 1 PM opening of parliament
+
+ggplot(subset(lix, type %in% c("openparl_dk", "openparl_no", "openparl_se")), 
+       aes(x = year, y = lix, colour = type)) +
+  geom_point() +
+  geom_smooth(se = FALSE) +
+  theme_classic() +
+  theme(legend.position = "bottom", 
+        legend.title = element_blank(),
+        panel.grid.minor = element_blank()) +
+  scale_colour_manual(breaks = c("openparl_dk", "openparl_se", "openparl_no"),
+                      values = c("red", "blue", "forestgreen"),
+                      labels = c("Danmark", "Sverige", "Norge")) +
+  labs(x = "År",
+       y = "Lixtal", 
+       title ="LIX for statsministerens åbningstaler", 
+       caption = "Data: github.com/straubinger/lix")
+
+ggsave("lix_openparl.png", height = 5, width = 7, dpi = 600)
+
+
+# Fig. 2 Royal New Year Address
+
+ggplot(subset(lix, type %in% c("newyear_royal_dk", "xmas_royal_se", "newyear_royal_no")), 
+       aes(x = year, y = lix, colour = type)) +
+  geom_point() +
+  geom_smooth(se = FALSE) +
+  theme_classic() +
+  theme(legend.position = "bottom", 
+        legend.title = element_blank(),
+        panel.grid.minor = element_blank()) +
+  scale_colour_manual(breaks = c("newyear_royal_dk", "xmas_royal_se", "newyear_royal_no"),
+                      values = c("red", "blue", "forestgreen"),
+                      labels = c("Danmark", "Sverige", "Norge")) +
+  labs(x = "År",
+       y = "Lixtal", 
+       title ="LIX for royale nytårstaler", 
+       caption = "Data: github.com/straubinger/lix")
+
+ggsave("lix_newyear_royal.png", height = 5, width = 7, dpi = 600)
+
+
+# Fig. 3 PM New Year Address
+
+ggplot(subset(lix, type %in% c("newyear_pm_dk", "newyear_pm_no")), 
+       aes(x = year, y = lix, colour = type)) +
+  geom_point() +
+  geom_smooth(se = FALSE) +
+  theme_classic() +
+  theme(legend.position = "bottom", 
+        legend.title = element_blank(),
+        panel.grid.minor = element_blank()) +
+  scale_colour_manual(breaks = c("newyear_pm_dk", "newyear_pm_no"),
+                      values = c("Red", "forestgreen"),
+                      labels = c("Danmark", "Norge")) +
+  xlim(1985, NA) +
+  labs(x = "År",
+       y = "Lixtal", 
+       title ="LIX for statsministerens nytårstaler", 
+       caption = "Data: github.com/straubinger/lix")
+
+ggsave("lix_newyear_pm.png", height = 5, width = 7, dpi = 600)
